7a7a4475561fa61e64cfcc1c13e154ba1519fdac
William.Phan <hoangviet191@gmail.com>
2021-09-05 21:25:20 +0700

    [PG] render a template

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Password Generator')
+    return render(request, 'generator/home.html')
