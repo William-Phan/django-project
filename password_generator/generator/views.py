96a1f3836c0362d597916923051556034dc7a27e
William.Phan <hoangviet191@gmail.com>
2021-09-06 10:43:43 +0700

    [PG] Generate password from form input; Return link to Home page

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,3 +1,5 @@
+import string
+import random
 from django.shortcuts import render
 
 
@@ -6,4 +8,21 @@ def home(request):
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+    characters = string.ascii_lowercase
+    length = int(request.GET.get('length'))
+    have_uppercase = request.GET.get('uppercase')
+    have_digit = request.GET.get('digit')
+    have_special_characters = request.GET.get('character')
+
+    if have_uppercase:
+        characters += string.ascii_uppercase
+    if have_digit:
+        characters += string.digits
+    if have_special_characters:
+        characters += string.punctuation
+
+    generated_password = [random.choice(characters) for _ in range(length)]
+    random.shuffle(generated_password)
+    generated_password = ''.join(generated_password)
+    return render(request, 'generator/password.html', {'password': generated_password})
+
