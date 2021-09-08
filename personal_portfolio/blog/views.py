a2ca1d843a4edc38251092b1cc783abfcf30cf3c
William.Phan <hoangviet191@gmail.com>
2021-09-08 18:44:00 +0700

    [PP] Add blog model and page, filtering (only show the specific number of latest blog)

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Blog
+
+
+def blog_home(request):
+    blogs = Blog.objects.all().order_by('-created_date')[:2]
+    context = {'blogs': blogs}
+    return render(request, 'blog/blog_home.html', context)
